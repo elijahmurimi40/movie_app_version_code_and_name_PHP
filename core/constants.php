fdead699922df98eda77433a870013a3463d7d97
Fortie40 <elijahmurimi40@gmail.com>
2020-11-24 09:59:27 +0300

    Update error.php page and other updates

--- core/constants.php
+++ core/constants.php
@@ -1,6 +1,7 @@
 <?php
 // error codes
 define('ERROR_CONNECTING_TO_DATABASE', 100);
-define('ERROR_GETTING_DATA', 102);
+define('ERROR_GETTING_DATA', 101);
 define('WRONG_PASSWORD', 102);
+define('ERROR', 103);
 define('SUCCESS', 200);
--- error.php
+++ error.php
@@ -1,2 +1,6 @@
 <?php
-echo 'error pol';
+require_once 'core/constants.php';
+require_once 'core/helper_functions.php';
+
+HelperFunctions::return_message('error', ERROR, 
+        'No page found');
